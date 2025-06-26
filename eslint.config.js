3572ce22d1b49a692d61a7e2dd1338ee632f85bc
Ark21bit <webleif@vk.com>
2025-06-26 16:58:36 +0300

    eslint fix

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,17 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
 	unocss: true,
 	vue: true,
+	typescript: true,
 	stylistic: {
 		indent: 'tab',
 	},
 	rules: {
 		'vue/block-order': ['error', {
-			order: [['script', 'template'], 'style'],
+			order: [
+				'template',
+				'script',
+				'style',
+			],
 		}],
 	},
 })
